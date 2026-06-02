9f45d6cd21a644989d633177904c8e9bd1e808d4
NICKOBREGON <36298813+NICKOBREGON@users.noreply.github.com>
2026-06-02 21:21:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'agenda-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=IBM+Plex+Sans:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.map(u => new Request(u, { cache: 'reload' })))).catch(() =>
+      caches.open(CACHE).then(c => c.add('./index.html'))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
